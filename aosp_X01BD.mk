a55b814383fefea7d9a55763cd7f933ab7fff5a3
SakilMondal <mondalsakil@gmail.com>
2020-11-23 12:21:40 +0530

    X01BD: Disable Live Wallpapers
    Change-Id: I0d7bab800ce7239467d66186b4820b2d06361d8d

--- aosp_X01BD.mk
+++ aosp_X01BD.mk
@@ -15,6 +15,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 # PixelExperience Properties
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
